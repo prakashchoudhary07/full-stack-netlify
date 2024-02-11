1341ba4a0e9d777ec945a428f1b3786da0806f4f
aps08 <anoopprsingh.social@gmail.com>
2024-02-11 16:34:32 +0530

    deplpyment ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(()=>{
+    fetch('/api/hello').then((res)=> res.json()).then((data)=> {
+      console.log(data);
+    })
+  }, []);
   return (
     <div className="App">
       <h1>Hellow World</h1>
